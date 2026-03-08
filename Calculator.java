fdeca4d48aa0ec87ff1311d900089d648e8553bb
unknown <vlad.hatos@gmail.com>
2026-03-08 15:19:31 +0200

    fix: Corecteaza valoarea bonusului la 5

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,7 @@ public class Calculator {
     // Calculeaza suma a doua numere
     public int aduna(int a, int b) {
         // Temporar, adunam si un bonus hardcodat
-        int bonus = 2;
+        int bonus = 5;
         return a + b + bonus;
     }
 
@@ -11,4 +11,3 @@ public class Calculator {
         System.out.println("Rezultat: " + calc.aduna(5, 10));
     }
 }
-
