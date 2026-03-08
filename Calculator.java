aa16ab4bf1e8e88de95e60aebf03ec9b9e283350
unknown <vlad.hatos@gmail.com>
2026-03-08 14:21:16 +0200

    feat: Adauga clasa initiala Calculator.

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+public class Calculator {
+    // Calculeaza suma a doua numere
+    public int aduna(int a, int b) {
+        // Temporar, adunam si un bonus hardcodat
+        int bonus = 2;
+        return a + b + bonus;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println("Rezultat: " + calc.aduna(5, 10));
+    }
+}
+
